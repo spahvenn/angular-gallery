845557808b0f04462e6ef1ae840a4aad19399bfe
Sami Ahvenniemi <spahvenn@gmail.com>
2025-03-31 12:51:42 +0300

    Added gallery link below images

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { CardListComponent } from './components/card-list/card-list.component';
 import { PageContentContainerComponent } from '../../layout/page-content-container/page-content-container.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { SeasonCardsComponent } from './components/season-cards/season-cards.component';
+import { MatAnchor } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,7 @@ import { SeasonCardsComponent } from './components/season-cards/season-cards.com
     PageContentContainerComponent,
     CarouselComponent,
     SeasonCardsComponent,
+    MatAnchor,
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
