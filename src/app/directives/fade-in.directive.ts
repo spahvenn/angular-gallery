d82ee484cf689294141e5f62cbe4b7615efe9cd0
Sami Ahvenniemi <spahvenn@gmail.com>
2025-03-31 12:24:50 +0300

    Made fade-in.directive work with any element

--- src/app/directives/fade-in.directive.ts
+++ src/app/directives/fade-in.directive.ts
@@ -8,28 +8,28 @@ export class FadeInDirective implements AfterViewInit, OnDestroy {
   @Input() fadeInDuration: number = 1.5;
   
   private observer!: IntersectionObserver;
-  private img: HTMLImageElement;
+  private element: HTMLElement;
 
   constructor(private el: ElementRef) {
-    this.img = this.el.nativeElement;
+    this.element = this.el.nativeElement;
   }
 
   ngAfterViewInit() {
-    this.img.style.opacity = '0';
-    this.img.style.transition = `opacity ${this.fadeInDuration}s ease`;
-    this.img.style.display = 'block';
+    this.element.style.opacity = '0';
+    this.element.style.transition = `opacity ${this.fadeInDuration}s ease`;
+    this.element.style.display = 'block';
 
     this.observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
-          this.img.style.opacity = '1';
-          this.observer.unobserve(this.img);
+          this.element.style.opacity = '1';
+          this.observer.unobserve(this.element);
         }
       },
       { threshold: 0.1 }
     );
 
-    this.observer.observe(this.img);
+    this.observer.observe(this.element);
   }
 
   ngOnDestroy() {
