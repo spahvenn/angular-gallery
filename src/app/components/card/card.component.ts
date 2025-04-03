46f66af2eacec144d5424d0d1d8afa60e472792d
Sami Ahvenniemi <spahvenn@gmail.com>
2025-04-03 10:03:04 +0300

    Add 'In cart' label to gallery images

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,21 +1,31 @@
 import { Component, inject, Input } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+import { NgIf, NgOptimizedImage } from '@angular/common';
 import { FadeInDirective } from '../../directives/fade-in.directive';
 import { ImageDialogComponent } from './components/image-dialog/image-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { StoreItem } from '../../data/store-items';
+import { CartService } from '../../services/cart/cart.service';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   templateUrl: './card.component.html',
-  imports: [NgOptimizedImage, FadeInDirective],
+  imports: [NgOptimizedImage, FadeInDirective, NgIf],
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
   readonly dialog = inject(MatDialog);
-
+  protected cartItemIds!: string[];
   @Input() storeItem!: StoreItem;
+  @Input() showInCartLabel = false;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.cart$.subscribe(cartItemIds => {
+      this.cartItemIds = cartItemIds;
+    });
+  }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(ImageDialogComponent, {
