931e0c5e3cc2d1c021acffe7c946d8faf99f725c
Sami Ahvenniemi <spahvenn@gmail.com>
2025-04-03 14:28:17 +0300

    Improve shopping cart visuals

--- src/app/pages/shopping-cart/components/shopping-cart-summary/components/checkout-button/checkout-button.component.ts
+++ src/app/pages/shopping-cart/components/shopping-cart-summary/components/checkout-button/checkout-button.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { MatButton } from '@angular/material/button';
+import { MatButton, MatFabButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-checkout-button',
   standalone: true,
-  imports: [MatButton],
+  imports: [MatButton, MatIcon, MatFabButton],
   templateUrl: './checkout-button.component.html',
   styleUrl: './checkout-button.component.scss',
 })
