291e1193c71ab6c91c89deb88fcb4aecbb733f02
Sami Ahvenniemi <spahvenn@gmail.com>
2025-04-07 13:32:23 +0300

    Added page meta titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,22 @@ import { HomeComponent } from './pages/home/home.component';
 import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'gallery', component: GalleryComponent },
-  { path: 'gallery/:season', component: GalleryComponent },
-  { path: 'shopping-cart', component: ShoppingCartComponent },
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Angular Gallery | Home',
+  },
+  {
+    path: 'gallery',
+    title: 'Angular Gallery | Gallery',
+    children: [
+      { path: '', component: GalleryComponent },
+      { path: ':season', title: 'Angular Gallery | Gallery', component: GalleryComponent },
+    ],
+  },
+  {
+    path: 'shopping-cart',
+    title: 'Angular Gallery | Shopping Cart',
+    component: ShoppingCartComponent,
+  },
 ];
