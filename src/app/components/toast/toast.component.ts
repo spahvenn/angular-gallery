354abdc1c8d973826511c446ce0692c0afbb96e5
Sami Ahvenniemi <spahvenn@gmail.com>
2025-04-08 09:53:15 +0300

    Toast visuals changed, removed auto dismiss and now has link to shopping cart

--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Toast, ToastService } from '../../services/toast/toast.service';
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+import { MatAnchor, MatButton } from '@angular/material/button';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-toast',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, MatIcon, MatAnchor, RouterLink, MatButton, NgIf],
   templateUrl: './toast.component.html',
   styleUrl: './toast.component.scss',
 })
 export class ToastComponent implements OnInit {
   toasts: Toast[] = [];
 
-  constructor(private toastService: ToastService) {}
+  constructor(
+    private toastService: ToastService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.toastService.toast$.subscribe((toast: Toast) => {
       this.toasts.push(toast);
-      setTimeout(() => this.removeToast(toast), toast.duration || 3000);
     });
   }
 
@@ -26,5 +31,14 @@ export class ToastComponent implements OnInit {
     setTimeout(() => {
       this.toasts = this.toasts.filter(t => t !== toast);
     }, 300);
+  }
+
+  onGoToCartButtonClick() {
+    this.toasts = [];
+    this.router.navigate(['/shopping-cart']);
+  }
+
+  isShoppingCartPage() {
+    return this.router.url === '/shopping-cart';
   }
 }
