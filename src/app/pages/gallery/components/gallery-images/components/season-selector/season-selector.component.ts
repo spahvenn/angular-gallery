12f7b65d0f8183460dd94e7c528899304c2661ed
Sami Ahvenniemi <spahvenn@gmail.com>
2025-04-11 10:18:26 +0300

    Fix season selector radio button keyboard navigation

--- src/app/pages/gallery/components/gallery-images/components/season-selector/season-selector.component.ts
+++ src/app/pages/gallery/components/gallery-images/components/season-selector/season-selector.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
 import { FormsModule } from '@angular/forms';
 import { Season } from '../../../../../../types/season.type';
@@ -11,13 +19,23 @@ import { Router } from '@angular/router';
   templateUrl: './season-selector.component.html',
   styleUrl: './season-selector.component.scss',
 })
-export class SeasonSelectorComponent {
+export class SeasonSelectorComponent implements AfterViewInit {
   @Input() selectedSeason: Season = 'All';
   @Output() seasonChange = new EventEmitter<Season>();
+  @ViewChildren(MatRadioButton) radioButtons!: QueryList<MatRadioButton>;
 
   seasons: Season[] = ['All', 'Spring', 'Summer', 'Autumn', 'Winter'];
 
   constructor(private router: Router) {}
+
+  ngAfterViewInit() {
+    // Focus the selected radio button after view init
+    // This fixes issue where keyboard navigation loses radio button focus
+    const selectedRadio = this.radioButtons.find(radio => radio.value === this.selectedSeason);
+    if (selectedRadio) {
+      selectedRadio.focus();
+    }
+  }
 
   onSeasonChange(season: Season) {
     this.seasonChange.emit(season);
