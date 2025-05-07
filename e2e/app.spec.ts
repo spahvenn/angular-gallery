c8ec2869c9f59ab5d2553ee38ec987be9350bce3
Sami Ahvenniemi <spahvenn@gmail.com>
2025-05-07 13:24:21 +0300

    Tests use shared wcag21aa criteria

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -1,42 +1,41 @@
-import { expect, test } from '@playwright/test';
-import AxeBuilder from '@axe-core/playwright';
+import { expect, test } from './axe-test';
 
-test('Home page', async ({ page }) => {
+test('Home page', async ({ page, makeAxeBuilder }) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('Home | Angular Gallery');
 
-  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+  const accessibilityScanResults = await makeAxeBuilder().analyze();
   expect(accessibilityScanResults.violations).toEqual([]);
 });
 
-test('Gallery page', async ({ page }) => {
+test('Gallery page', async ({ page, makeAxeBuilder }) => {
   await page.goto('/gallery');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('Photo Gallery | Angular Gallery ');
 
-  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+  const accessibilityScanResults = await makeAxeBuilder().analyze();
   expect(accessibilityScanResults.violations).toEqual([]);
 });
 
-test('Shopping cart page', async ({ page }) => {
+test('Shopping cart page', async ({ page, makeAxeBuilder }) => {
   await page.goto('/shopping-cart');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('Shopping Cart | Angular Gallery');
 
-  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+  const accessibilityScanResults = await makeAxeBuilder().analyze();
   expect(accessibilityScanResults.violations).toEqual([]);
 });
 
-test('About page', async ({ page }) => {
+test('About page', async ({ page, makeAxeBuilder }) => {
   await page.goto('/about');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('About | Angular Gallery');
 
-  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+  const accessibilityScanResults = await makeAxeBuilder().analyze();
   expect(accessibilityScanResults.violations).toEqual([]);
 });
---
+++ e2e/axe-test.ts
@@ -0,0 +1,18 @@
+import { test as base } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+
+type AxeFixture = {
+  makeAxeBuilder: () => AxeBuilder;
+};
+
+// Extend base test by providing "makeAxeBuilder"
+//
+// This new "test" can be used in multiple test files, and each of them will get
+// a consistently configured AxeBuilder instance.
+export const test = base.extend<AxeFixture>({
+  makeAxeBuilder: async ({ page }, use) => {
+    const makeAxeBuilder = () => new AxeBuilder({ page }).withTags(['wcag21aa']);
+    await use(makeAxeBuilder);
+  },
+});
+export { expect } from '@playwright/test';
